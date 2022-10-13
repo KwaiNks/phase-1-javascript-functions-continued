4cdf955e6a2137c23645087fb9b34337cb9d096f
Richmond Nkansah <richmondnkansah@gmail.com>
2022-10-12 21:54:20 -0400

    commit tree 633651cb84e0f676184e163a038c11a809aa057f

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function saturdayFun(defValue = "roller-skate"){
+    return `This Saturday, I want to ${defValue}!`;
+}
+
+function mondayWork(value = "go to the office"){
+   return `This Monday, I will ${value}.`;
+}
+
+function wrapAdjective(outerValue = "*"){
+    return function(innerValue = "special"){
+        return `You are ${outerValue}${innerValue}${outerValue}!`;
+    }
+
+}
